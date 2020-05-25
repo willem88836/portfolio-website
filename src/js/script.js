44804e8db25f32b62a343051291fe3357e7a85a3
Willem Meijer <willem1998m@hotmail.com>
2020-05-25 11:58:57 +0200

    Moved JS code to script.js
    HTML files now only initialize their scripts instead of loading everything themselves. 
    content loading is now simplified.

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,32 @@ function httpGet(theUrl)
     xmlHttp.send( null );
     return xmlHttp.responseText;
 }
+
+
+function loadRawContent(elementId, contentPath) {
+    document.getElementById(elementId).innerHTML = httpGet(contentURL + contentPath);
+}
+
+
+function initializeResume() {
+    loadRawContent("short-resume", "texts/short-resume.html");
+}
+
+function initializeProblemSolver(){
+    loadRawContent("spraac-description", "projects/spraac-description.html");
+    loadRawContent("intoreality-description", "projects/intoreality-description.html");
+    loadRawContent("samenwerkingsspel-description", "projects/samenwerkingsspel-description.html");
+}
+
+function initializeDeveloper() {
+    loadRawContent("short-developer", "texts/short-developer.html");
+    
+    loadRawContent("code-display-py", "sourcecode/GameOfLife.py")
+    loadRawContent("commentary-game-of-life", "sourcecode/commentary-GameOfLife.html");
+
+    loadRawContent("code-display-cs", "sourcecode/QuickSort.cs");
+    loadRawContent("commentary-quick-sort", "sourcecode/commentary-QuickSort.html");
+
+    loadRawContent("code-display-java", "sourcecode/KruskalsAlgorithm.java");
+    loadRawContent("commentary-kruskals-algorithm", "sourcecode/commentary-KruskalsAlgorithm.html");
+}
